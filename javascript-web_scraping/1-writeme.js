360105f7451d51995fb80bd1e0440c35cbd7389b
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-02 22:37:42 +0100

    format fix

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,8 +2,13 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], String(process.argv[3]), 'utf8', function (err, data) {
+fs.writeFile(
+  process.argv[2],
+  String(process.argv[3]),
+  'utf8',
+  function (err, data) {
     if (err) {
-        console.log(err);
-      }
-});
+      console.log(err);
+    }
+  }
+);

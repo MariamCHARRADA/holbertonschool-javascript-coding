b509a7d0a0e1fc7d9666a31f8755a12979cdb314
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-02 22:36:22 +0100

    fix task two beugs adding console.log(err)

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,6 +2,8 @@
 
 const fs = require('fs');
 
-fs.writeFile(process.argv[2], String(process.argv[3]),'utf8',function (err, data) {
-  
+fs.writeFile(process.argv[2], String(process.argv[3]), 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+      }
 });

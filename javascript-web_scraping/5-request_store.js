21ed8c85ff069c3104510425d2004d59421d3556
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-03 00:06:42 +0100

    Loripsum

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+request.get(process.argv[2],'UTF-8', (error, response, body) => {
+    if (error) {
+        console.log(error);
+    } else {
+        fs.writeFile(process.argv[3], body, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+})

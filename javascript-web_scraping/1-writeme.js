e3cae888003bd9d7268ae3a34af2656e69696896
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-02 22:29:05 +0100

    delete console.log from task two

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,9 +3,5 @@
 const fs = require('fs');
 
 fs.writeFile(process.argv[2], String(process.argv[3]),'utf8',function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+  
 });

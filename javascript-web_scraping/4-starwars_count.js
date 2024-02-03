25dc65ab18b152fd9b7cae265832267a200e3595
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-03 01:29:18 +0100

    change logic code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,29 @@
 
 const request = require('request');
 
-request.get(process.argv[2],
+const apiUrl = process.argv[2];
+
+request.get(apiUrl,
     function (error, response, body) {
         if (error) {
-            console.log(error);
+            console.error(error);
         } else {
-            const films = JSON.parse(body).results;
-            console.log(films.filter((film) => film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')).length);
+            try {
+                const films = JSON.parse(body);
+                const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
+
+                const count = films.results.reduce((acc, film) => {
+                    if (film.characters.includes(characterUrl)) {
+                        return acc + 1;
+                    } else {
+                        return acc;
+                    }
+                }, 0);
+
+                console.log(count);
+            } catch (parseError) {
+                console.error('Error parsing JSON:', parseError);
+            }
         }
     }
-)
+);

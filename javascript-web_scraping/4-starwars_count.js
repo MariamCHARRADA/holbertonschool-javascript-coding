b52523e84060e42ce838ee8f191d4ef611483147
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-04 14:37:19 +0100

    hot fix

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,30 +1,24 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 
 const apiUrl = process.argv[2];
 
-request.get(apiUrl,
-    function (error, response, body) {
-        if (error) {
-            console.error(error);
-        } else {
-            try {
-                const films = JSON.parse(body);
-                const characterUrl = 'https://swapi-api.hbtn.io/api/people/18/';
-
-                const count = films.results.reduce((acc, film) => {
-                    if (film.characters.includes(characterUrl)) {
-                        return acc + 1;
-                    } else {
-                        return acc;
-                    }
-                }, 0);
-
-                console.log(count);
-            } catch (parseError) {
-                console.error('Error parsing JSON:', parseError);
-            }
+request.get(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const characterUrl = "https://swapi-api.hbtn.io/api/people/18/";
+    let count = 0;
+    for (const film of films) {
+      const characters = film.characters;
+      for (const character of characters) {
+        if (character === characterUrl) {
+          count++;
         }
+      }
     }
-);
+    console.log(count);
+  }
+});

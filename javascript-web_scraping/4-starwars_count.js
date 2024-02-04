90e2a01811f5ed4bc9484952c439bbc3d79c75e9
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-04 14:41:59 +0100

    includes 18

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -4,17 +4,16 @@ const request = require("request");
 
 const apiUrl = process.argv[2];
 
-request(apiUrl, function (error, response, body) {
+request.get(apiUrl, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
     const films = JSON.parse(body).results;
-    const characterUrl = "https://swapi-api.hbtn.io/api/people/18/";
     let count = 0;
     for (const film of films) {
       const characters = film.characters;
       for (const character of characters) {
-        if (character === characterUrl) {
+        if (character.includes('18')) {
           count++;
         }
       }

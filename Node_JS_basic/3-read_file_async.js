674e8fd74878d6aa9d5ab0ed6f51186751ed1abf
MariamCHARRADA <charradamariam@yahoo.com>
2024-03-31 22:40:41 +0100

    Reading a file asynchronously

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error(`Cannot load the database: ${err.message}`));
+      } else {
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        let startIndex = 0;
+        if (
+          lines.length > 0
+          && lines[0].startsWith('firstname,lastname,age,field')
+        ) {
+          startIndex = 1;
+        }
+
+        const counters = {};
+
+        for (let i = startIndex; i < lines.length; i += 1) {
+          const [firstName, , , field] = lines[i].split(',');
+          if (field) {
+            counters[field] = counters[field] || { count: 0, names: [] };
+            counters[field].count += 1;
+            counters[field].names.push(firstName.trim());
+          }
+        }
+
+        console.log(`Number of students: ${lines.length - startIndex}`);
+        for (const field in counters) {
+          if (Object.prototype.hasOwnProperty.call(counters, field)) {
+            console.log(
+              `Number of students in ${field}: ${
+                counters[field].count
+              }. List: ${counters[field].names.join(', ')}`,
+            );
+          }
+        }
+
+        resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;

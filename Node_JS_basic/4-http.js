eef45876828bfb7db316abae8f6376602f928d6d
MariamCHARRADA <charradamariam@yahoo.com>
2024-03-31 22:48:37 +0100

    HTTP module

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!\n');
+});
+
+const PORT = 1245;
+app.listen(PORT);
+module.exports = app;

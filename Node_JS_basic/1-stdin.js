9b3df9a7ca8bb797b8e2906a609eb7d0ff6db1b3
MariamCHARRADA <charradamariam@yahoo.com>
2024-03-31 17:53:11 +0100

    Using Process stdin

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,5 @@
+console.log("Welcome to Holberton School, what is your name?");
+process.stdin.on("data", (name) => {
+  console.log(`Your name is: ${name}This important software is now closing`);
+  process.exit();
+});
